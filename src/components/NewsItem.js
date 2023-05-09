fe892592cf8230ea3e6747392d4fdd55bb4b1147
Shehryar-Abbasi <sheryaabbasi264@gmail.com>
2023-05-09 17:19:23 +0500

    initial commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+                        <span className=" badge rounded-pill bg-danger" >
+                            {source}
+                        </span>
+                    </div>
+                    <img src={!imageUrl ? "https://image.cnbcfm.com/api/v1/image/107236486-1683302006613-gettyimages-1232683502-BOOKING_EARNS.jpeg?v=1683378001&w=1920&h=1080" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}..</h5>
+                        <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
